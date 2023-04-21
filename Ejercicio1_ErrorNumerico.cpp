80b5bc8439c890024849a3e57f8f84b07b5451cd
Kelvin Melgar <kelvinmelgar777@gmail.com>
2023-04-21 14:31:44 -0600

    Otro

--- Ejercicio1_ErrorNumerico.cpp
+++ Ejercicio1_ErrorNumerico.cpp
@@ -1,8 +1,48 @@
-
 #include <iostream>
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
     /* code */
+
+    do
+    {
+        cout << "Lab 1 Pro 3" << endl;
+        cout << endl << "1. Error Numerico. \n";
+        cout << "2. Division de 2 entradas. \n\n";
+        cout << "3. Salir" << endl << endl;
+
+        cout << "Ingrese una opcion [1, 2, 3]: ";
+        int opcion;
+        cin >> opcion;
+        
+        switch (opcion)
+        {
+        case 1 :
+            
+
+            break;
+        
+        case 2 :
+
+            break;
+
+        case 3 :
+
+            break;
+
+        default:
+            cout << "\nOpcion invalida...\n\n";
+            break;
+        }
+
+
+
+        
+    } while (/* condition */);
+    
+
+
+
     return 0;
 }
